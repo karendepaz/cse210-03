a7d3c401d8494c6134d5ab72d2244c98f1213371
karendepaz <karendeyanira02@gmail.com>
2022-05-24 12:58:04 -0500

    completed file
    Secret word is working

--- secret_word.py
+++ secret_word.py
@@ -23,16 +23,18 @@ class SecretWord:
         """
         Construct the object with necessary attributes.
         """
-        pass
+        self.__word = self.set_word()
+        self.guessed_letters = []
+        self.letters_left = 0
+        self.guessed_wrong = 0
 
     def set_word(self):
         """
         Selects and returns a random word from the list.
         """
-        # Create the list of words that will be picked from.
-
-        # Select and return a random word from the list.
-        pass
+        word = ["summer", "heat", "pool", "sun"]
+        picked = random.choice(word)
+        return picked
 
     def show_word(self):
         """
@@ -42,33 +44,39 @@ class SecretWord:
 
         # Reset 'letters_left' attribute to 0 in order to get
         # a fresh count.
-        
+        self.letters_left = 0
 
         # Print each letter in the word that has already been
         # guessed. Leave letters that have yet to be guessed
         # as '_'.
         print("The word is...")
-        
-                
+        for letter in self.__word:
+            if letter in self.guessed_letters:
+                print(letter, end=" ")
             # If the letter has not been guessed, print '_'
             # and add 1 to the 'letters_left'.
-            pass
+            else:
+                print("_", end=" ")
+                self.letters_left += 1
 
     def check_guess(self, guess):
 
         # Check if the player has already guessed the letter that
         # they entered.
-        
-
-        # Else...
-        
-
-            # If the letter the player guessed is not in the secret word, inform
+        if guess in self.guessed_letters:
+            print("You already guessed that letter. Try another letter.")
+        else:
+             # If the letter the player guessed is not in the secret word, inform
             # the player and add to their 'guessed_wrong' count. Add
             # the guessed letter to the 'guessed_letters' list.
-            
-                
+            if guess in self.__word:
+                self.guessed_letters.append(guess)
+                print("Good guess!")
+             
             # If the letter the player guessed is in the secret word,
             # inform the player. Add the guessed letter to the 
             # 'guessed_letters' list.
-            pass
+            else: 
+                self.guessed_letters.append(guess)
+                print("Wrong guess!")
+                self.guessed_wrong += 1
